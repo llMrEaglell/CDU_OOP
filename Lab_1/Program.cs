3a9b7a7c8a8fc7be561738555f570c463e311ffc
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-09-19 15:40:32 +0300

    Finish task

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -6,7 +6,88 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Task2
+            try
+            {
+                TTriangle triangle = new TTriangle(10, 6, 8);
+                Console.WriteLine(triangle.ToString());
+                Console.WriteLine(triangle.perimetr());
+                Console.WriteLine(triangle.square());
+            }
+            catch (NullReferenceException e)
+            {
+
+            }
+            catch
+            {
+                Console.WriteLine("Triangle does not exist ");
+            }
+
+            //Task 1
+            Console.WriteLine("Input number of rows:");
+            int rows = int.Parse(Console.ReadLine());
+            int a, b;
+            do
+            {
+                Console.WriteLine("Input start and end:");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+            } while (a > b);
+
+            Random rand = new Random();
+            int[][] toothedArray = new int[rows][];
+        generate:
+            int count = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                toothedArray[i] = new int[rand.Next(a, b)];
+                count += toothedArray[i].Length;
+            }
+
+            Console.WriteLine("Count element's:" + count);
+            if (!(Math.Sqrt(count) == Math.Truncate(Math.Sqrt(count))))
+            {
+                Console.WriteLine("The number of elements is not a square of a natural number\n" +
+                                  "Press SPACE to generate new array");
+                if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+                    goto generate;
+            }
+
+            foreach (var t in toothedArray)
+            {
+                for (int j = 0; j < t.Length; j++)
+                    t[j] = rand.Next(100);
+            }
+
+            int[] simpleArray = new int[count];
+            int counter = 0;
+            foreach (int[] elem in toothedArray)
+            {
+                foreach (int num in elem)
+                {
+                    Console.Write("{0,4}", num);
+                    simpleArray[counter] = num;
+                    counter++;
+                }
+
+                Console.WriteLine();
+            }
+
+            Array.Sort(simpleArray);
+            int size = (int)Math.Sqrt(count);
+            int[,] quadArray = new int[size, size]; //квадратна
+            counter = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    quadArray[i, j] = simpleArray[counter];
+                    counter++;
+                    Console.Write("{0:00} ", quadArray[i, j]);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Lab_1/TTriangle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using static System.Math;
+
+namespace Lab_1
+{
+    class TTriangle
+    {
+        protected double _hypotenuse;
+        protected double _opposite;
+        protected double _adjacent;
+
+        public TTriangle(double hypotenuse, double opposite, double adjacent)
+        {
+            if (opposite + adjacent > hypotenuse && adjacent + hypotenuse > opposite &&
+                hypotenuse + opposite > adjacent && opposite > 0 && adjacent > 0 && hypotenuse > 0)
+            {
+                this.hypotenuse = hypotenuse;
+                this.opposite = opposite;
+                this.adjacent = adjacent;
+            }
+            else throw new Exception("Triangle does not exist ");
+        }
+
+        public double hypotenuse
+        {
+            get { return _hypotenuse; }
+            set
+            {
+                if ((opposite + adjacent > value && adjacent + value > opposite && value + opposite > adjacent &&
+                     opposite > 0 && adjacent > 0 && value > 0) || hypotenuse == 0 || opposite == 0 ||
+                    adjacent == 0) _hypotenuse = value;
+                else throw new Exception("Triangle does not exist ");
+            }
+        }
+
+        public double opposite
+        {
+            get { return _opposite; }
+            set
+            {
+                if ((value + adjacent > hypotenuse && adjacent + hypotenuse > value && hypotenuse + value > adjacent &&
+                     value > 0 && adjacent > 0 && hypotenuse > 0) || hypotenuse == 0 || opposite == 0 ||
+                    adjacent == 0) _opposite = value;
+                else throw new Exception("Triangle does not exist ");
+            }
+        }
+
+        public double adjacent
+        {
+            get { return _adjacent; }
+            set
+            {
+                if ((opposite + value > hypotenuse && value + hypotenuse > opposite && hypotenuse + opposite > value &&
+                     opposite > 0 && value > 0 && hypotenuse > 0) || hypotenuse == 0 || opposite == 0 ||
+                    adjacent == 0) _adjacent = value;
+                else throw new Exception("Triangle does not exist ");
+            }
+        }
+
+        public double perimetr()
+        {
+            return hypotenuse + opposite + adjacent;
+        }
+
+        public double square()
+        {
+            double perimetr_2 = perimetr() / 2;
+            return Sqrt(perimetr_2 * (perimetr_2 - opposite) * (perimetr_2 - adjacent) * (perimetr_2 - hypotenuse));
+        }
+
+        public override string ToString()
+        {
+            return "Hypotenuse:" + hypotenuse + "\nOpposite:" + opposite + "\nAdjacent:" + adjacent + "\n";
+        }
+
+    }
+}
