401c6107fde3cdb81e705fe5e6a2e200e742ce7c
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-09-28 19:27:16 +0300

    Realise method for take count columns

--- Lab_2/MyMatrix.cs
+++ Lab_2/MyMatrix.cs
@@ -1,7 +1,56 @@
-﻿namespace Lab_2
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab_2
 {
     public class MyMatrix
     {
+        private double[,] matrix;
+
+        public int Height
+        {
+            get { return this.matrix.Length; }
+        }
+        public int Width
+        {
+            get { return 0; }
+        }
+
+        public MyMatrix(MyMatrix inputMatrix)
+        {
+            this.matrix = inputMatrix.matrix;
+        }
+
+        public MyMatrix(double[,] inputMatrix)
+        {
+            this.matrix = inputMatrix;
+        }
+
+        public MyMatrix(double[][] inputDoubles)
+        {
+            try
+            {
+                foreach (double[] arr in inputDoubles)
+                {
+                    if (inputDoubles.Length != arr.Length) throw new Exception("The number of rows and columns do not match");
+                }
+                int size = inputDoubles.Length;
+                this.matrix = new double[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        this.matrix[i,j] = inputDoubles[i][j];
+                    }
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Matrix is not a quad");
+            }
+        }
+
+        
         
     }
 }
