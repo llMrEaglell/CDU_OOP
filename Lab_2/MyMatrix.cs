075c6498ba44bb3ba3d400aa564123846bb1084d
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-09-30 13:17:29 +0300

    Fix Heigh&Width. Test code'

--- Lab_2/MyMatrix.cs
+++ Lab_2/MyMatrix.cs
@@ -6,14 +6,24 @@ namespace Lab_2
     public class MyMatrix
     {
         private double[,] matrix;
-
         public int Height
         {
-            get { return this.matrix.Length; }
+            get => this.matrix.GetLength(0);
         }
+
         public int Width
+    {
+            get => this.matrix.GetLength(1);
+        }
+
+        public int getHeight()
         {
-            get { return 0; }
+            return Height;
+        }
+
+        public int getWidth()
+        {
+            return Width;
         }
 
         public MyMatrix(MyMatrix inputMatrix)
@@ -32,25 +42,46 @@ namespace Lab_2
             {
                 foreach (double[] arr in inputDoubles)
                 {
-                    if (inputDoubles.Length != arr.Length) throw new Exception("The number of rows and columns do not match");
+                    if (inputDoubles.Length != arr.Length)
+                        throw new Exception("The number of rows and columns do not match");
                 }
+
                 int size = inputDoubles.Length;
                 this.matrix = new double[size, size];
                 for (int i = 0; i < size; i++)
                 {
                     for (int j = 0; j < size; j++)
                     {
-                        this.matrix[i,j] = inputDoubles[i][j];
+                        this.matrix[i, j] = inputDoubles[i][j];
                     }
                 }
             }
-            catch 
+            catch
             {
                 Console.WriteLine("Matrix is not a quad");
             }
         }
 
-        
-        
+        // TODO Конструктор з масиву рядків
+
+        public double this[int x, int y] // Індексатор, для зверення як до масиву
+        {
+            get { return matrix[x, y]; }
+            set { this.matrix[x, y] = value; }
+        }
+
+        public static MyMatrix operator * (MyMatrix matrix1, MyMatrix matrix2)
+        {
+            // TODO Перевірка чи можливо перемножити
+
+            double[,] result = new double[matrix1.Height,matrix2.Width];
+
+            return new MyMatrix(result);
+        }
+
+        override public String ToString()
+        {
+            return "";
+        }
     }
 }
--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -6,7 +6,23 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            int m, n;
+            m = 10;
+            n = 4;
+            double[,] arr = new double[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    arr[i, j] = i + j;
+                }
+            }
+            MyMatrix a = new MyMatrix(arr);
+
+            Console.WriteLine(a.Height);
+            Console.WriteLine(a.Width);
+
         }
     }
 }
