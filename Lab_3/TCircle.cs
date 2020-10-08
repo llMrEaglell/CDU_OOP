f4bfa6fd22ef3fd465e518888b8e721103a1d5fc
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-10-08 12:21:47 +0300

    Crate class TCircle. Create constructor. Override toString

---
+++ Lab_3/TCircle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab_3
+{
+    public class TCircle
+    {
+        private double Radius
+        {
+            get => Radius;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException(nameof(value));
+                else this.Radius=value;
+            }
+        }
+
+        public TCircle()
+        {
+            this.Radius = 0;
+        }
+        public TCircle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public TCircle(TCircle circle)
+        {
+            this.Radius = circle.Radius;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Radius:{0}",this.Radius);
+        }
+    }
+}
---
+++ Lab_3/Triangle.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab_3
+{
+    public class Triangle
+    {
+        
+    }
+}
