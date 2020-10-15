8cd3670bff20f922cc84e67863e955b0d4c3cb08
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-10-15 11:22:01 +0300

    Change operator + to found distance between points

--- Lab_3/Point.cs
+++ Lab_3/Point.cs
@@ -1,7 +1,29 @@
-﻿namespace Lab_3
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Lab_3
 {
     public class Point
     {
-        
+        private int X
+        {
+            get => X;
+            set => X = value;
+        }
+
+        private int Y
+        {
+            get => Y;
+            set => Y = value;
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static double operator +(Point point1, Point point2) =>
+            Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
     }
 }
