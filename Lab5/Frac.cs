793d5e3deb3feb906e64598434b160d96a2f1069
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-12-16 14:59:50 +0200

    Init class Frac

--- Lab5/Frac.cs
+++ Lab5/Frac.cs
@@ -1,7 +1,51 @@
-﻿namespace Lab5
+﻿using System.Numerics;
+
+namespace Lab5
 {
-    public class Frac
+    public class Frac : IMyNumber<Frac>
     {
-        
+        private BigInteger _nom;
+        private BigInteger _denom;
+
+        public BigInteger Nom
+        {
+            get => _nom;
+            set => _nom = value;
+        }
+
+        public BigInteger Denom
+        {
+            get => _denom;
+            set => _denom = value;
+        }
+
+        public Frac(BigInteger nom, BigInteger denom)
+        {
+            _nom = nom;
+            _denom = denom;
+        }
+
+        public Frac Add(Frac obj)
+        {
+            return new Frac(
+                BigInteger.Add(BigInteger.Multiply(this.Nom, obj.Denom),
+                    BigInteger.Multiply(this.Denom, obj.Nom)),
+                BigInteger.Multiply(this.Denom, obj.Nom));
+        }
+
+        public Frac Substract(Frac obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Frac Multiply(Frac obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Frac Devide(Frac obj)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
