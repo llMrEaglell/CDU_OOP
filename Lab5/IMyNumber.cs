86d0765ee59da0448353422be7862c7ecc38ebc2
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-12-16 14:05:37 +0200

    Create interface for number

---
+++ Lab5/IMyNumber.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5
+{
+    public interface IMyNumber<T> where T : IMyNumber<T>
+    {
+        T Add(T obj);
+        T Substract(T obj);
+        T Multiply(T obj);
+        T Devide(T obj);
+    }
+}
