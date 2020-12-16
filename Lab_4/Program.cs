d6246319fa332d33c125c4db4e0b6b38f155660f
Oleksandr Hubenko <sasha0420011@gmail.com>
2020-12-16 13:55:09 +0200

    Add Bitmap. Finish task2.

--- Lab_4/Program.cs
+++ Lab_4/Program.cs
@@ -7,8 +7,11 @@ namespace Lab_4
     {
         static void Main(string[] args)
         {
-            Task1 task1 = new Task1();
+            /*Task1 task1 = new Task1();
             task1.Method();
+            Console.ReadKey();*/    
+            Task2 task2 = new Task2();
+            task2.method();
         }
     }
 }
---
+++ Lab_4/Task2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace Lab_4
+{
+    public class Task2
+    {
+        private static readonly string DefaultPathDir = $@"{Directory.GetCurrentDirectory()}\pict\";
+
+        private int count = 0;
+
+        public void method()
+        {
+            Regex regex = new Regex("^((.bmp)|(.gif)|(.tiff?)|(.jpe?g)|(.png))$", RegexOptions.IgnoreCase);
+            string[] picts = Directory.GetFiles(DefaultPathDir);
+            
+            foreach (string pict in picts)
+            {
+                try
+                {
+                    if (regex.IsMatch(Path.GetExtension(pict)))
+                    {
+                        Bitmap bitmap = new Bitmap(pict);
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
+                        string fileName = Path.GetFileNameWithoutExtension(pict);
+                        bitmap.Save(DefaultPathDir + fileName + "-mirrored.gif");
+                        bitmap.Dispose();
+                        count++;
+                    }
+                    else
+                        throw new Exception($"File [{Path.GetFileName(pict)}] not a image!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
