104b1e8205552ce3f84f8864a1d08af1b5ca0b9f
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-03-10 11:50:53 +0200

    Code clean up

--- Lab_7_task2/Form1.cs
+++ Lab_7_task2/Form1.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab_2_New_Task2
 {
     public delegate void Opacity(double x);
-    
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -56,36 +50,36 @@ namespace Lab_2_New_Task2
         private void button3_Click(object sender, EventArgs e)
         {
             MessageBox.Show(
-                "Hello World!", 
-                "Повідомлення", 
-                MessageBoxButtons.OK, 
+                "Hello World!",
+                "Повідомлення",
+                MessageBoxButtons.OK,
                 MessageBoxIcon.Information);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             MessageBox.Show(
-                "Я супермегакнопка,\n i цього мене не позбавиш!", 
-                "Повідомлення", 
-                MessageBoxButtons.OK, 
+                "Я супермегакнопка,\n i цього мене не позбавиш!",
+                "Повідомлення",
+                MessageBoxButtons.OK,
                 MessageBoxIcon.Information);
-            
+
             if (checkBox1.Checked)
             {
                 ChangeOpacity();
             }
-            
+
             if (checkBox2.Checked)
             {
                 ChangeBGColor();
             }
-            
+
             if (checkBox3.Checked)
             {
                 MessageBox.Show(
-                    "Hello World!", 
-                    "Повідомлення", 
-                    MessageBoxButtons.OK, 
+                    "Hello World!",
+                    "Повідомлення",
+                    MessageBoxButtons.OK,
                     MessageBoxIcon.Information);
             }
         }
