3b0ab477e1b9fd85abf2aac164fd0cdc43dd4552
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-03-17 12:23:59 +0200

    Finish task 1 lab 7

---
+++ Lab7_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace Lab7_1
+{
+    class Program
+    {
+        delegate void TimerExecutor(int sec);
+
+        static void Main(string[] args)
+        {
+            Write("Enter count of seconds: ");
+            int sec = int.Parse(ReadLine());
+            Timer timer = new Timer();
+            TimerExecutor del = timer.Start;
+            del.Invoke(sec);
+        }
+    }
+}
---
+++ Lab7_1/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace Lab7_1
+{
+    class Timer
+    {
+        private const int MilsecI = 1000;
+
+        Action[] methods =
+        {
+            RandomNumber,
+            CurrentTime,
+            MouthDay
+        };
+
+        public void Start(int sec)
+        {
+            Random rand = new Random();
+            while (true)
+            {
+                Thread.Sleep(sec * MilsecI);
+                int a = rand.Next(0, 3);
+                methods[a].Invoke();
+            }
+        }
+
+        private static void MouthDay()
+        {
+            WriteLine("Mouth day: " + DateTime.Now.Day);
+        }
+
+        private static void CurrentTime()
+        {
+            WriteLine("Currnet time: " + DateTime.Now.TimeOfDay);
+        }
+
+        private static void RandomNumber()
+        {
+            Random rand = new Random();
+            int a = rand.Next(0, 100);
+            WriteLine("Random number: " + a);
+        }
+    }
+}
