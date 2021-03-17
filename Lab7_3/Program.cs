eb7e2f21dca6beeb5a2dc58576e343daa96811fa
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-03-17 12:40:44 +0200

    Fish task 3

--- Lab7_3/Program.cs
+++ Lab7_3/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace Lab7_3
 {
     class Program
     {
+        private delegate void DivideOn(int[] mas);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] mas = {30, 1, 18, 45, 79, 65, 48, 31, 155, 165, 17, 7, 1, 3, 33, 11, 33, 77, 121, 171};
+            DivideOn d3 = IfDividedOn3;
+            DivideOn d7 = IfDividedOn7;
+            d3.Invoke(mas);
+            d7.Invoke(mas);
+        }
+
+        private static void IfDividedOn3(int[] mas)
+        {
+            WriteLine("Divide on 3: ");
+            foreach (int a in mas)
+                if (a % 3 == 0)
+                {
+                    Write(a + " ");
+                }
+
+            WriteLine();
+        }
+
+        private static void IfDividedOn7(int[] mas)
+        {
+            WriteLine("Divide on 7: ");
+            foreach (int a in mas)
+            {
+                if (a % 7 == 0)
+                {
+                    Write(a + " ");
+                }
+            }
+
+            WriteLine();
         }
     }
 }
--- Lab_7_task2/Form1.Designer.cs
+++ Lab_7_task2/Form1.Designer.cs
@@ -40,9 +40,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(56, 20);
+            this.button1.Location = new System.Drawing.Point(42, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 31);
+            this.button1.Size = new System.Drawing.Size(80, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "Прозорість";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,9 +51,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(169, 20);
+            this.button2.Location = new System.Drawing.Point(127, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 31);
+            this.button2.Size = new System.Drawing.Size(80, 25);
             this.button2.TabIndex = 1;
             this.button2.Text = "Колір тла";
             this.button2.UseVisualStyleBackColor = true;
@@ -60,9 +62,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(282, 20);
+            this.button3.Location = new System.Drawing.Point(212, 16);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(104, 31);
+            this.button3.Size = new System.Drawing.Size(78, 25);
             this.button3.TabIndex = 2;
             this.button3.Text = "Hello World";
             this.button3.UseVisualStyleBackColor = true;
@@ -70,9 +73,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(56, 57);
+            this.button4.Location = new System.Drawing.Point(42, 46);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(330, 37);
+            this.button4.Size = new System.Drawing.Size(248, 30);
             this.button4.TabIndex = 3;
             this.button4.Text = "супермегакнопка";
             this.button4.UseVisualStyleBackColor = true;
@@ -80,36 +84,42 @@
             // 
             // checkBox1
             // 
-            this.checkBox1.Location = new System.Drawing.Point(56, 119);
+            this.checkBox1.Location = new System.Drawing.Point(42, 97);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(330, 24);
+            this.checkBox1.Size = new System.Drawing.Size(248, 20);
             this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "\"супермегакнопка\" поглинає \"Прозорість\"";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // checkBox2
             // 
-            this.checkBox2.Location = new System.Drawing.Point(56, 149);
+            this.checkBox2.Location = new System.Drawing.Point(42, 121);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(330, 24);
+            this.checkBox2.Size = new System.Drawing.Size(248, 20);
             this.checkBox2.TabIndex = 5;
             this.checkBox2.Text = "\"супермегакнопка\" поглинає \"Колір тла\"\r\n\r\n";
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox3
             // 
-            this.checkBox3.Location = new System.Drawing.Point(56, 179);
+            this.checkBox3.Location = new System.Drawing.Point(42, 145);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(330, 24);
+            this.checkBox3.Size = new System.Drawing.Size(248, 20);
             this.checkBox3.TabIndex = 6;
             this.checkBox3.Text = "\"супермегакнопка\" поглинає \"Hello World\"";
             this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(441, 304);
+            this.ClientSize = new System.Drawing.Size(331, 247);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
@@ -117,6 +127,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
