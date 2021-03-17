901584f2583a96637f51e932fe0c3a51b531d84a
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-03-17 11:39:33 +0200

    Fix to not ifed

--- Lab_7_task1/Program.cs
+++ Lab_7_task1/Program.cs
@@ -5,7 +5,6 @@ namespace Lab_2_New
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             WriteLine("Enter number 0, 1 or 2 for operations and number(Example: 0 7 or 1 3)\n" +
@@ -24,24 +23,14 @@ namespace Lab_2_New
                     op = Convert.ToInt32(subs[0]);
                     num = Convert.ToDouble(subs[1]);
 
-                    Action<double> oper;
-                    if (op == 0)
+                    Action[] actions =
                     {
-                       oper = Root;
-                       Operation(num, oper); 
-                    } else if (op == 1)
-                    {
-                        oper = Triple;
-                        Operation(num, oper);
-                    } else if (op == 2)
-                    {
-                        oper = Addition;
-                        Operation(num, oper);
-                    }
-                    else
-                    {
-                        break;
-                    }
+                        () => Root(num),
+                        () => Triple(num),
+                        () => Addition(num)
+                    };
+
+                    actions[op].Invoke();
                 }
                 catch (Exception ex)
                 {
@@ -51,21 +40,16 @@ namespace Lab_2_New
             }
         }
 
-        private static void Operation(double x, Action<double> op)
-        {
-            op(x);
-        }
-        
         private static void Root(double x)
         {
             WriteLine(Math.Sqrt(Math.Abs(x)));
         }
-        
+
         private static void Triple(double x)
         {
             WriteLine(Math.Pow(x, 3));
         }
-        
+
         private static void Addition(double x)
         {
             WriteLine(x + 3.5);
