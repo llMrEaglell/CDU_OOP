970e9d25284bc274fc0ce8b3b9574a70404de3a5
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-03-17 12:32:16 +0200

    Rename package

---
+++ Lab7_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
