3caf59a329cee78b9697f26905a095cb23eea947
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-03-31 10:59:45 +0300

    question

---
+++ Lab8/WіnQuestіon/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WіnQuestіon_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyes = new System.Windows.Forms.Button();
+            this.btnno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnyes
+            // 
+            this.btnyes.Location = new System.Drawing.Point(67, 30);
+            this.btnyes.Name = "btnyes";
+            this.btnyes.Size = new System.Drawing.Size(70, 28);
+            this.btnyes.TabIndex = 0;
+            this.btnyes.Text = "Так";
+            this.btnyes.UseVisualStyleBackColor = true;
+            this.btnyes.Click += new System.EventHandler(this.btnyes_Click);
+            // 
+            // btnno
+            // 
+            this.btnno.Location = new System.Drawing.Point(222, 30);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(70, 28);
+            this.btnno.TabIndex = 1;
+            this.btnno.Text = "Ні";
+            this.btnno.UseVisualStyleBackColor = true;
+            this.btnno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnno_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(95, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ви задоволені своєю заробітною платою?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 153);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnno);
+            this.Controls.Add(this.btnyes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Нагальне питання";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button btnyes;
+        private System.Windows.Forms.Button btnno;
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ Lab8/WіnQuestіon/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WіnQuestіon_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnyes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mи й не сумнівалися, що Ви так думаєте!");
+        }
+
+        private void btnno_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnno.Top -= e.Y;
+            btnno.Left += e.X;
+            if (btnno.Top < -10 || btnno.Top > 100) btnno.Top = 60;
+            if (btnno.Left < -80 || btnno.Left > 250) btnno.Left = 120;
+        }
+    }
+}
