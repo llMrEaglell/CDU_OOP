d5fca08a09758229df94f6d9a42ba176561e0ca5
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-03-31 11:36:19 +0300

    TestList

---
+++ Lab8/TestList/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TestList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.peopleList = new System.Windows.Forms.ListBox();
+            this.memberList = new System.Windows.Forms.CheckedListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.peopleList);
+            this.groupBox1.Controls.Add(this.memberList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 183);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список учасників";
+            // 
+            // peopleList
+            // 
+            this.peopleList.FormattingEnabled = true;
+            this.peopleList.ItemHeight = 16;
+            this.peopleList.Items.AddRange(new object[] {"Зоря В. П.", "Губенко О. В."});
+            this.peopleList.Location = new System.Drawing.Point(146, 35);
+            this.peopleList.Name = "peopleList";
+            this.peopleList.Size = new System.Drawing.Size(91, 52);
+            this.peopleList.TabIndex = 4;
+            // 
+            // memberList
+            // 
+            this.memberList.FormattingEnabled = true;
+            this.memberList.Location = new System.Drawing.Point(20, 35);
+            this.memberList.Name = "memberList";
+            this.memberList.Size = new System.Drawing.Size(120, 89);
+            this.memberList.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Додати";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(108, 12);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(80, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Видалити";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(205, 12);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(86, 23);
+            this.buttonSort.TabIndex = 3;
+            this.buttonSort.Text = "Сортувати";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 259);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Робота зі списками";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.ListBox peopleList;
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSort;
+
+        private System.Windows.Forms.CheckedListBox memberList;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+
+        #endregion
+    }
+}
---
+++ Lab8/TestList/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TestList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (peopleList.Text.Length != 0)
+            {
+                memberList.Items.Add(peopleList.Text);
+            }
+            else MessageBox.Show("Bиберіть елемент зі списку або введіть новий");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            while (memberList.CheckedIndices.Count > 0)
+            {
+                memberList.Items.RemoveAt(memberList.CheckedIndices[0]);
+            }
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            memberList.Sorted = true;
+        }
+    }
+}
